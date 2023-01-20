66f3ace90ffc9d703710677dd86856a0b43f7643
Lucky <117139436+LuckyBelieve@users.noreply.github.com>
2023-01-20 20:34:14 +0200

    revision on node js

--- revision2.js
+++ revision2.js
@@ -6,3 +6,25 @@ lucky.on('username logged in',(e) =>{
           console.log('you have logged in your account',e);
 })
 lucky.mynames('Ineza Lucky Believe');
+
+
+// let's write another promise code
+
+const userLeft = false;
+const userWatchingCatMeme = false;
+
+function watchingtutorialpromise(){
+    return new Promise((resolve, reject) => {
+        if(userLeft){
+            reject('user left');
+        }
+        if (userWatchingCatMeme) {
+            reject('user Watching Cat Meme')
+        } else {
+            resolve('the user is watching tutorials' )            
+        }
+    })
+}
+watchingtutorialpromise().then((message)=>{
+    console.log(message);
+})
