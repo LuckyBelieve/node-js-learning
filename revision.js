3560e971ef281debe0d12f985820fce9a2141ab4
Lucky <117139436+LuckyBelieve@users.noreply.github.com>
2023-01-26 22:40:52 +0200

    today's revision about node.js

--- revision.js
+++ revision.js
@@ -1,49 +1,49 @@
-// //this is about to learn about promises
+//this is about to learn about promises
 
-// let p = new Promise((resolve, reject) => {
-//     let myName = 'ineza';
-//     if(myName =='ineza'){
-//         resolve('this username is correct');
-//     }else{
-//         reject('this username is  incorrect');
-//     }
-// })
-// p.then((message) =>{
-//     console.log(message);
-// }).catch((message) =>{
-//     console.log(message);
-// })
+let p = new Promise((resolve, reject) => {
+    let myName = 'ineza';
+    if(myName =='ineza'){
+        resolve('this username is correct');
+    }else{
+        reject('this username is  incorrect');
+    }
+})
+p.then((message) =>{
+    console.log(message);
+}).catch((message) =>{
+    console.log(message);
+})
 
-// // let's start with reminding ourselves how to raise an event and how to handle that event in the node
+// let's start with reminding ourselves how to raise an event and how to handle that event in the node
 
-// const EventEmitter = require('events');
-//  let myName = "Ineza Lucky Believe";
-// class Lucky extends EventEmitter{
-//     mynames(names){
-//         console.log('my names are '+names);
-//         this.emit('username logged in',{id:200,Name:'ineza lucky'});
-//     }
-// }
+const EventEmitter = require('events');
+ let myName = "Ineza Lucky Believe";
+class Lucky extends EventEmitter{
+    mynames(names){
+        console.log('my names are '+names);
+        this.emit('username logged in',{id:200,Name:'ineza lucky'});
+    }
+}
 
-// module.exports = Lucky;
+module.exports = Lucky;
 
-// const http = require('http');
+const http = require('http');
 
-//   const server = http.createServer((req,res)=>{
-//     if(req.url === '/'){
-//         res.write('Hello '+myName);
-//         res.end();
-//     }
-//     if(req.url === '/api/courses'){
-//         res.write(myName+' you\'re welcome to courses\' page');
-//         res.end();
-//     }
+  const server = http.createServer((req,res)=>{
+    if(req.url === '/'){
+        res.write('Hello '+myName);
+        res.end();
+    }
+    if(req.url === '/api/courses'){
+        res.write(myName+' you\'re welcome to courses\' page');
+        res.end();
+    }
 
     
-// })
-// server.listen('5000')
+})
+server.listen('5000')
 
-// console.log('the server is listening on port 5000');
+console.log('the server is listening on port 5000');
 
 // we are going to create a class and try to listen to the event when it's raised
 
